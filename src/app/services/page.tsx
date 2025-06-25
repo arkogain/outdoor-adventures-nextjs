3df17738fb701e0d5b0523529eb5c95e9af9f2bb
Abhijit saha <work.abhijitsaha@gmail.com>
2025-06-25 11:42:37 +0530

    services page changes

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,3 +1,4 @@
+import { Minus } from "lucide-react";
 import { Metadata } from "next";
 import Image from "next/image";
 
@@ -16,67 +17,127 @@ const page = () => {
         </div>
       </div>
       {/*  */}
+      <div className="container mx-auto px-6">
+        <div className="flex flex-col items-center gap-7 py-12 lg:py-24">
+          <div className="text-center text-2xl font-bold md:text-3xl lg:text-4xl">{`It's Time to Start Your Adventures`}</div>
+          <div className="h-0.5 w-24 bg-rose-500"></div>
+          <div className="w-full text-center text-sm md:w-4/5 lg:w-3/5 lg:text-base">
+            Click edit button to change this text. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Ut elit tellus, luctus ullamcorpe
+            pulvinar.
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 place-items-center gap-10 text-center md:grid-cols-2">
+          {/* 1st */}
+          <div className="flex w-full flex-col items-center gap-6 md:w-4/5 lg:w-4/6">
+            <Image
+              src={"/services/backpacking.jpg"}
+              alt="bagpacking image"
+              height={500}
+              width={500}
+              className="w-full rounded shadow-[10px_10px_0px_.3px_rgba(0,0,0,0.15)]"
+            />
+            <div className="text-lg font-bold lg:text-xl">
+              Backpacking Trips
+            </div>
+            <div className="">
+              Fuerat aestu carentem habentia spectent tonitrua mutastis locavit
+              liberioris inistra possedit.
+            </div>
+          </div>
+          {/* 2nd */}
+          <div className="flex w-full flex-col items-center gap-6 md:w-4/5 lg:w-4/6">
+            <Image
+              src={"/services/family-hiking.jpg"}
+              alt="bagpacking image"
+              height={500}
+              width={500}
+              className="w-full rounded shadow-[10px_10px_0px_.3px_rgba(0,0,0,0.15)]"
+            />
+            <div className="text-lg font-bold lg:text-xl">
+              Family Hiking Trips
+            </div>
+            <div className="">
+              Fuerat aestu carentem habentia spectent tonitrua mutastis locavit
+              liberioris inistra possedit.
+            </div>
+          </div>
+          {/* 3rd */}
+          <div className="flex w-full flex-col items-center gap-6 md:w-4/5 lg:w-4/6">
+            <Image
+              src={"/services/water-sports.jpg"}
+              alt="bagpacking image"
+              height={500}
+              width={500}
+              className="w-full rounded shadow-[10px_10px_0px_.3px_rgba(0,0,0,0.15)]"
+            />
+            <div className="text-lg font-bold lg:text-xl">Water Sports</div>
+            <div className="">
+              Fuerat aestu carentem habentia spectent tonitrua mutastis locavit
+              liberioris inistra possedit.
+            </div>
+          </div>
+          {/* 4th */}
+          <div className="flex w-full flex-col items-center gap-6 md:w-4/5 lg:w-4/6">
+            <Image
+              src={"/services/winter-sports.jpg"}
+              alt="bagpacking image"
+              height={500}
+              width={500}
+              className="w-full rounded shadow-[10px_10px_0px_.3px_rgba(0,0,0,0.15)]"
+            />
+            <div className="text-lg font-bold lg:text-xl">Winter Sports</div>
+            <div className="">
+              Fuerat aestu carentem habentia spectent tonitrua mutastis locavit
+              liberioris inistra possedit.
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* */}
       <div className="">
-        <div className="">{`It's Time to Start Your Adventures`}</div>
         <div className="">
-          Click edit button to change this text. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Ut elit tellus, luctus ullamcorpe
-          pulvinar.
-        </div>
-        {/* 1st */}
-        <div className="">
-          <Image
-            src={"/services/backpacking.jpg"}
-            alt="bagpacking image"
-            height={500}
-            width={500}
-          />
-          <div className="">Backpacking Trips</div>
+          <div className="text-2xl font-bold md:text-3xl lg:text-4xl">{`It's Time to Start Your Adventures`}</div>
+          <div className="h-0.5 w-24 bg-rose-500"></div>
+          <div className="w-full text-sm md:w-4/5 lg:w-3/5 lg:text-base">
+            Click edit button to change this text. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Ut elit tellus, luctus ullamcorpe
+            pulvinar.
+          </div>
+          {/*  */}
           <div className="">
-            Fuerat aestu carentem habentia spectent tonitrua mutastis locavit
-            liberioris inistra possedit.
+            <div className="flex gap-3">
+              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
+            </div>
+            <div className="flex gap-3">
+              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
+            </div>
+            <div className="flex gap-3">
+              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
+            </div>
           </div>
         </div>
-        {/* 2nd */}
+        {/*  */}
         <div className="">
-          <Image
-            src={"/services/family-hiking.jpg"}
-            alt="bagpacking image"
-            height={500}
-            width={500}
-          />
-          <div className="">Family Hiking Trips</div>
-          <div className="">
-            Fuerat aestu carentem habentia spectent tonitrua mutastis locavit
-            liberioris inistra possedit.
+          <div className="text-2xl font-bold md:text-3xl lg:text-4xl">{`It's Time to Start Your Adventures`}</div>
+          <div className="h-0.5 w-24 bg-rose-500"></div>
+          <div className="w-full text-sm md:w-4/5 lg:w-3/5 lg:text-base">
+            Click edit button to change this text. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Ut elit tellus, luctus ullamcorpe
+            pulvinar.
           </div>
-        </div>
-        {/* 3rd */}
-        <div className="">
-          <Image
-            src={"/services/water-sports.jpg"}
-            alt="bagpacking image"
-            height={500}
-            width={500}
-          />
-          <div className="">Water Sports</div>
+          {/*  */}
           <div className="">
-            Fuerat aestu carentem habentia spectent tonitrua mutastis locavit
-            liberioris inistra possedit.
-          </div>
-        </div>
-        {/* 4th */}
-        <div className="">
-          <Image
-            src={"/services/winter-sports.jpg"}
-            alt="bagpacking image"
-            height={500}
-            width={500}
-          />
-          <div className="">Winter Sports</div>
-          <div className="">
-            Fuerat aestu carentem habentia spectent tonitrua mutastis locavit
-            liberioris inistra possedit.
+            <div className="flex gap-3">
+              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
+            </div>
+            <div className="flex gap-3">
+              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
+            </div>
+            <div className="flex gap-3">
+              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
+            </div>
           </div>
         </div>
       </div>
