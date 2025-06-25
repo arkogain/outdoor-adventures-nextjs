28e1b18e6e286c5f4a17f67d16ddbde1050f39e3
Arko Gain <work.abhijitsaha@gmail.com>
2025-06-25 14:27:58 +0530

    services page done!

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
     <>
       <div className="bg-[url('/services/services-bg.jpg')] bg-cover bg-center bg-no-repeat">
         <div className="bg-black/30">
-          <div className="py-32 text-center text-3xl font-bold text-white sm:text-4xl lg:text-6xl">
+          <div className="py-44 text-center text-3xl font-bold text-white sm:text-4xl lg:text-6xl">
             Services
           </div>
         </div>
@@ -94,49 +94,57 @@ const page = () => {
             </div>
           </div>
         </div>
-      </div>
-      {/* */}
-      <div className="">
-        <div className="">
-          <div className="text-2xl font-bold md:text-3xl lg:text-4xl">{`It's Time to Start Your Adventures`}</div>
-          <div className="h-0.5 w-24 bg-rose-500"></div>
-          <div className="w-full text-sm md:w-4/5 lg:w-3/5 lg:text-base">
-            Click edit button to change this text. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Ut elit tellus, luctus ullamcorpe
-            pulvinar.
+        {/* */}
+        <div className="grid grid-cols-1 gap-10 space-y-5 py-12 md:grid-cols-2 lg:py-24">
+          <div className="space-y-5">
+            <div className="text-xl font-bold sm:text-nowrap md:text-2xl lg:text-4xl">
+              Why Outdoor Adventure
+            </div>
+            <div className="h-0.5 w-24 bg-rose-500"></div>
+            <div className="w-full text-sm md:w-4/5 lg:w-3/5 lg:text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+            </div>
+            {/*  */}
+            <div className="space-y-2">
+              <div className="flex gap-3">
+                <Minus color="#ff2056" /> Experience: Locavit liberioris
+                possedit
+              </div>
+              <div className="flex gap-3">
+                <Minus color="#ff2056" />
+                Reputation: Diremit mundi mare undae
+              </div>
+              <div className="flex gap-3">
+                <Minus color="#ff2056" /> Guide Experience: Spectent tonitrua
+                mutastis
+              </div>
+            </div>
           </div>
           {/*  */}
-          <div className="">
-            <div className="flex gap-3">
-              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
+          <div className="space-y-5">
+            <div className="text-xl font-bold md:text-2xl lg:text-4xl">
+              What You Get
             </div>
-            <div className="flex gap-3">
-              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
+            <div className="h-0.5 w-24 bg-rose-500"></div>
+            <div className="w-full text-sm md:w-4/5 lg:w-3/5 lg:text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
             </div>
-            <div className="flex gap-3">
-              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
-            </div>
-          </div>
-        </div>
-        {/*  */}
-        <div className="">
-          <div className="text-2xl font-bold md:text-3xl lg:text-4xl">{`It's Time to Start Your Adventures`}</div>
-          <div className="h-0.5 w-24 bg-rose-500"></div>
-          <div className="w-full text-sm md:w-4/5 lg:w-3/5 lg:text-base">
-            Click edit button to change this text. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Ut elit tellus, luctus ullamcorpe
-            pulvinar.
-          </div>
-          {/*  */}
-          <div className="">
-            <div className="flex gap-3">
-              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
-            </div>
-            <div className="flex gap-3">
-              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
-            </div>
-            <div className="flex gap-3">
-              <Minus color="#ff2056" /> Experience: Locavit liberioris possedit
+            {/*  */}
+            <div className="space-y-2">
+              <div className="flex gap-3">
+                <Minus color="#ff2056" /> Private Hike: Locavit liberioris
+                possedit
+              </div>
+              <div className="flex gap-3">
+                <Minus color="#ff2056" /> Transportation: Diremit mundi mare
+                undae
+              </div>
+              <div className="flex gap-3">
+                <Minus color="#ff2056" /> Great Facilities: Spectent tonitrua
+                mutastis
+              </div>
             </div>
           </div>
         </div>
