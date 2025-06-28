9e6657de26afe81901349f1d021c94781fb01f36
Abhijit saha <work.abhijitsaha@gmail.com>
2025-06-28 17:47:58 +0530

    contact page 98% done..

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
+import Contactdetails from "@/components/Contactdetails";
 import ContactForm from "@/components/Forms/ContactForm";
-import { FacebookIcon, TwitterIcon, Youtube } from "lucide-react";
+import Contacthero from "@/components/Hero/Contacthero";
 import { Metadata } from "next";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Contact || outdoor-adventures-nextjs",
@@ -10,59 +10,11 @@ export const metadata: Metadata = {
 const page = () => {
   return (
     <>
-      <div className="bg-[url('/contact/contact-bg.jpg')] bg-cover bg-center bg-no-repeat">
-        <div className="bg-black/30">
-          <div className="py-40 text-center text-3xl font-bold text-white sm:text-4xl lg:py-64 lg:text-6xl">
-            Contact Us
-          </div>
-        </div>
-      </div>
-      <div className="">
-        <ContactForm />
-        {/*  */}
-        <div className="">
-          <div className="">Contact Info</div>
-          <div className="">
-            <div className="">Address​</div>
-            <div className="">123 Fifth Avenue, NY 10160, New York, USA</div>
-          </div>
-          <div className="">
-            <div className="">Email Us</div>
-            <div className="">contact@example.com</div>
-          </div>
-          <div className="">
-            <div className="">Call Us</div>
-            <div className="">800-123-456</div>
-          </div>
-          <div className="">
-            <div className="">Follow Us</div>
-            <div className="flex">
-              <Link
-                href={""}
-                className="rounded-full bg-rose-500 p-2 hover:bg-black ">
-                <FacebookIcon
-                  fill="white"
-                  stroke="none"
-                />
-              </Link>
-              <Link
-                href={""}
-                className="rounded-full bg-rose-500 p-2 hover:bg-black ">
-                <TwitterIcon
-                  fill="white"
-                  stroke="none"
-                />
-              </Link>
-              <Link
-                href={""}
-                className="rounded-full bg-rose-500 p-2 hover:bg-black ">
-                <Youtube
-                  fill="white"
-                  stroke="none"
-                />
-              </Link>
-            </div>
-          </div>
+      <Contacthero />
+      <div className="container mx-auto px-6">
+        <div className="grid grid-cols-1  gap-10 space-y-10 py-14 lg:grid-cols-2">
+          <ContactForm />
+          <Contactdetails />
         </div>
       </div>
     </>
---
+++ src/components/Hero/Contacthero.tsx
@@ -0,0 +1,15 @@
+const Contacthero = () => {
+  return (
+    <>
+      <div className="bg-[url('/contact/contact-bg.jpg')] bg-cover bg-center bg-no-repeat">
+        <div className="bg-black/30">
+          <div className="py-40 text-center text-3xl font-bold text-white sm:text-4xl lg:py-64 lg:text-6xl">
+            Contact Us
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contacthero;
