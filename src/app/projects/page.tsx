7ca9a378d1b6ee0af81913b0768ba7d863151ab6
Abhijit saha <work.abhijitsaha@gmail.com>
2025-07-03 17:12:01 +0530

    project hero add

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,4 @@
+import Projecthero from "@/components/Hero/Projecthero";
 import Slider1 from "@/components/Projects-Slide/Slider1";
 import Slider2 from "@/components/Projects-Slide/Slider2";
 import Slider3 from "@/components/Projects-Slide/Slider3";
@@ -10,61 +11,66 @@ export const metadata: Metadata = {
 
 const page = () => {
   return (
-    <section className="bg-[#f5f5f5]">
-      <div className="mx-auto max-w-7xl space-y-14 px-6 py-24 lg:space-y-10">
-        {/* slider 1  */}
-        <div className="space-y-5">
-          <div className="flex items-center gap-4">
-            <div className="font-primary text-[15px] font-bold">Adventure</div>
-            {/* border section  */}
-            <div className="bg-radical-red h-0.5 w-12"></div>
-          </div>
-
-          <div className="font-primary text-4xl font-bold">Water Sports</div>
-          <div className="font-secondary text-[16px] text-black/75">
-            Interdum exercitation penatibus, praesentium facilisi accusamus
-            fermentum, sagittis.
-          </div>
-          <Slider1 />
-        </div>
-
-        {/* slider 2  */}
-        <div className="space-y-5">
-          <div className="flex items-center gap-4">
-            <div className="font-primary text-[15px] font-bold">
-              Snow Adventure
+    <>
+      <Projecthero />
+      <section className="bg-[#f5f5f5]">
+        <div className="mx-auto max-w-7xl space-y-14 px-6 py-24 lg:space-y-10">
+          {/* slider 1  */}
+          <div className="space-y-5">
+            <div className="flex items-center gap-4">
+              <div className="font-primary text-[15px] font-bold">
+                Adventure
+              </div>
+              {/* border section  */}
+              <div className="bg-radical-red h-0.5 w-12"></div>
             </div>
-            {/* border section  */}
-            <div className="bg-radical-red h-0.5 w-12"></div>
-          </div>
 
-          <div className="font-primary text-4xl font-bold">Winter Sports</div>
-          <div className="font-secondary text-[16px] text-black/75">
-            Interdum exercitation penatibus, praesentium facilisi accusamus
-            fermentum, sagittis.
-          </div>
-          <Slider2 />
-        </div>
-
-        {/* slider 3  */}
-        <div className="space-y-5">
-          <div className="flex items-center gap-4">
-            <div className="font-primary text-[15px] font-bold">
-              Forest Tours
+            <div className="font-primary text-4xl font-bold">Water Sports</div>
+            <div className="font-secondary text-[16px] text-black/75">
+              Interdum exercitation penatibus, praesentium facilisi accusamus
+              fermentum, sagittis.
             </div>
-            {/* border section  */}
-            <div className="bg-radical-red h-0.5 w-12"></div>
+            <Slider1 />
           </div>
 
-          <div className="font-primary text-4xl font-bold">Camping</div>
-          <div className="font-secondary text-[16px] text-black/75">
-            Interdum exercitation penatibus, praesentium facilisi accusamus
-            fermentum, sagittis.
+          {/* slider 2  */}
+          <div className="space-y-5">
+            <div className="flex items-center gap-4">
+              <div className="font-primary text-[15px] font-bold">
+                Snow Adventure
+              </div>
+              {/* border section  */}
+              <div className="bg-radical-red h-0.5 w-12"></div>
+            </div>
+
+            <div className="font-primary text-4xl font-bold">Winter Sports</div>
+            <div className="font-secondary text-[16px] text-black/75">
+              Interdum exercitation penatibus, praesentium facilisi accusamus
+              fermentum, sagittis.
+            </div>
+            <Slider2 />
           </div>
-          <Slider3 />
+
+          {/* slider 3  */}
+          <div className="space-y-5">
+            <div className="flex items-center gap-4">
+              <div className="font-primary text-[15px] font-bold">
+                Forest Tours
+              </div>
+              {/* border section  */}
+              <div className="bg-radical-red h-0.5 w-12"></div>
+            </div>
+
+            <div className="font-primary text-4xl font-bold">Camping</div>
+            <div className="font-secondary text-[16px] text-black/75">
+              Interdum exercitation penatibus, praesentium facilisi accusamus
+              fermentum, sagittis.
+            </div>
+            <Slider3 />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
